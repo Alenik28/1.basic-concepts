de0ad378af5f7096a6343e4b94f7a34f15da757e
Solomakhin Anton <alena.dar@mail.ru>
2021-08-31 00:47:37 +0300

    first commit variables

---
+++ task.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function solveEquation(a, b, c) {
+  let arr = [];
+  let discriminant = Math.pow(b, 2) - 4*a*c;
+  if (discriminant === 0) {
+    arr.push(- b / (2 * a));
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  } 
+  return arr; 
+}
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  // код для задачи №2 писать здесь
+
+  return totalAmount;
+}
